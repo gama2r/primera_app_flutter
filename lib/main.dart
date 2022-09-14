0248d3e0280c2c18f5fb1c5b2c0a14ab57e1c4a3
Gamaliel Rodriguez <gamalielrodriguezroque@gmail.com>
2022-09-14 16:08:05 -0500

    agregando la dependencia de english_words

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,14 +10,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Bienvenidos a flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Bienvenido a flutter'),
         ),
-        body: const Center(
-          child: Text('Hola a todos'),
+        body: /*const*/ Center(
+          //child: Text('Hola a todos'),
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
